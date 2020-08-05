65f994df9bfd841b402d6b09701d90eae870bced
hersson ceballos <herssonceballos@gmail.com>
2020-08-05 15:56:34 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,16 +1,12 @@
 #write your code here
 
-
-
-
+number = 10 
 
 def countdown
-
-  #while 
-  
-  
-  
-  #end
+  while number < 10
+   puts "#{number} SECOND(S)!"
+   number -= 1
+    end
   
   
 end
